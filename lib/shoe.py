b9342af5a69c3a2ffe53de3da8a7117a10082eb8
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-08-23 23:04:44 +0300

    Mission Passed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+
+    @property
+    def brand(self):
+        return self._brand
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, new_size):
+      if new_size is not type(int):
+          print("size must be an integer")
+      else:
+          self._size = new_size
